a31736de1bb5b53bb6aa54e1b777bc1ae5c8253b
Dinh Vu Anh Thuy <anthdv2410@gmail.com>
2025-10-21 10:50:24 +0700

    feat: Implement add student function

---
+++ 36_DinhVuAnhThuy.py
@@ -0,0 +1,16 @@
+# 36_DinhVuAnhThuy.py
+
+# Danh sách để lưu thông tin các sinh viên.
+# Mỗi sinh viên là một dictionary.
+student_list = []
+
+def add_student(name, year_of_birth, address):
+    """
+    YÊU CẦU 1: Hoàn thiện hàm này.
+    - Tạo một dictionary để lưu thông tin sinh viên.
+    - Thêm dictionary đó vào danh sách `student_list`.
+    - In ra thông báo "Da them sinh vien <ten> thanh cong."
+    """
+    student = dict(name=name, year_of_birth=year_of_birth, address=address)
+    student_list.append(student)
+    print(f"Da them sinh vien {name} thanh cong.")
